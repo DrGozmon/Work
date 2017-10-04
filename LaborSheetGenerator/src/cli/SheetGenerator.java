8b7ef87c4e79608af26666384a153b9f36417548
alosi <alosi@WR1387.wrinternal01.com>
2017-10-04 14:49:05 -0400

    Finished sheet generator

--- LaborSheetGenerator/src/cli/SheetGenerator.java
+++ LaborSheetGenerator/src/cli/SheetGenerator.java
@@ -1,5 +1,6 @@
 package cli;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -10,11 +11,13 @@ import java.util.Scanner;
 import javax.swing.JFileChooser;
 
 import org.apache.poi.ss.usermodel.BorderStyle;
-import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -23,6 +26,8 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class SheetGenerator {
 
 	private static final String rates = "H:\\StaffRates.csv";
+	private static final String[] columns = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ", "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BK", "BL", "BM", "BN", "BO", "BP", "BQ", "BR", "BS", "BT", "BU", "BV", "BW", "BX", "BY", "BZ", "CA", "CB", "CC"};
+
 
 	private static ArrayList<String[]> readRates() throws FileNotFoundException {
 		ArrayList<String[]> ret = new ArrayList<String[]>();
@@ -37,8 +42,8 @@ public class SheetGenerator {
 		in.close();
 		return ret;
 	}
-
-	private static void generateSheet( String projectFileName ) {
+	
+	private static void generateSheet( String projectFileName, ArrayList<String[]> tasks ) {
 		try {
 			ArrayList<String[]> staffRates = readRates();
 			String filename = projectFileName.replaceAll( "csv", "xlsx" );
@@ -59,21 +64,21 @@ public class SheetGenerator {
 
 			XSSFFont boldFont = workbook.createFont();
 			boldFont.setFontHeightInPoints( ( short ) 10 );
-			boldFont.setFontName( "Calibri" );
+			boldFont.setFontName( "Lato" );
 			boldFont.setColor( IndexedColors.BLACK.getIndex() );
 			boldFont.setBold(true);
 			boldFont.setItalic(false);
 
 			XSSFFont italicFont = workbook.createFont();
 			italicFont.setFontHeightInPoints( ( short ) 10 );
-			italicFont.setFontName( "Calibri" );
+			italicFont.setFontName( "Lato" );
 			italicFont.setColor( IndexedColors.BLACK.getIndex() );
 			italicFont.setBold(false);
 			italicFont.setItalic(true);
 
 			XSSFFont boldItalicFont = workbook.createFont();
 			boldItalicFont.setFontHeightInPoints( ( short ) 10 );
-			boldItalicFont.setFontName( "Calibri" );
+			boldItalicFont.setFontName( "Lato" );
 			boldItalicFont.setColor( IndexedColors.BLACK.getIndex() );
 			boldItalicFont.setBold(true);
 			boldItalicFont.setItalic(true);
@@ -85,36 +90,125 @@ public class SheetGenerator {
 			headerFont.setBold(true);
 			headerFont.setItalic(false);
 
-			CellStyle greenStyle = workbook.createCellStyle();
+			XSSFCellStyle greenStyle = workbook.createCellStyle();
 			greenStyle.setFont( headerFont );
-			/* byte[] rgb = new byte[3];
-		    rgb[0] = (byte) 122; // red
-		    rgb[1] = (byte) 171; // green
-		    rgb[2] = (byte) 101; // blue
-		    greenStyle.setFillForegroundColor( new XSSFColor( rgb, new DefaultIndexedColorMap() ).getIndex() );
-		    greenStyle.setFillPattern( FillPatternType.SOLID_FOREGROUND );*/
+		    greenStyle.setFillForegroundColor( new XSSFColor( new Color( 122, 171, 101 ) ) );
+		    greenStyle.setFillPattern( FillPatternType.SOLID_FOREGROUND );
 			greenStyle.setAlignment( HorizontalAlignment.CENTER );
 			greenStyle.setBorderLeft( BorderStyle.MEDIUM );
 			greenStyle.setBorderRight( BorderStyle.MEDIUM );
 			greenStyle.setBorderTop( BorderStyle.MEDIUM );
 			greenStyle.setBorderBottom( BorderStyle.MEDIUM );
+			greenStyle.setVerticalAlignment( VerticalAlignment.CENTER );
 
-			CellStyle greyStyle = workbook.createCellStyle();
-			greyStyle.setFont( defaultFont );
-			greyStyle.setFillForegroundColor( IndexedColors.GREY_25_PERCENT.getIndex() );
-			greyStyle.setFillPattern( FillPatternType.SOLID_FOREGROUND );
+			XSSFCellStyle whiteTaskStyle = workbook.createCellStyle();
+			whiteTaskStyle.setFont( defaultFont );
+			whiteTaskStyle.setAlignment( HorizontalAlignment.CENTER );
+			whiteTaskStyle.setBorderLeft( BorderStyle.THIN );
+			whiteTaskStyle.setBorderRight( BorderStyle.THIN );
+			whiteTaskStyle.setBorderTop( BorderStyle.THIN );
+			whiteTaskStyle.setBorderBottom( BorderStyle.THIN );
+			
+			XSSFCellStyle greyTaskStyle = workbook.createCellStyle();
+			greyTaskStyle.cloneStyleFrom( whiteTaskStyle );
+			greyTaskStyle.setFillForegroundColor( new XSSFColor( new Color( 237, 237, 237 ) ) );
+			greyTaskStyle.setFillPattern( FillPatternType.SOLID_FOREGROUND );
+			
+			XSSFCellStyle whiteFeeStyle = workbook.createCellStyle();
+			whiteFeeStyle.cloneStyleFrom( whiteTaskStyle );
+			whiteFeeStyle.setDataFormat( (short) 8 );
+			
+			XSSFCellStyle greyFeeStyle = workbook.createCellStyle();
+			greyFeeStyle.cloneStyleFrom( greyTaskStyle );
+			greyFeeStyle.setDataFormat( (short) 8 );
+			
+			XSSFCellStyle whiteTaskHeaderStyle = workbook.createCellStyle();
+			whiteTaskHeaderStyle.setFont( boldItalicFont );
+			whiteTaskHeaderStyle.setBorderLeft( BorderStyle.THIN );
+			whiteTaskHeaderStyle.setBorderRight( BorderStyle.MEDIUM );
+			whiteTaskHeaderStyle.setBorderTop( BorderStyle.THIN );
+			whiteTaskHeaderStyle.setBorderBottom( BorderStyle.THIN );
+			whiteTaskHeaderStyle.setAlignment( HorizontalAlignment.LEFT );
+			
+			XSSFCellStyle greyTaskHeaderStyle = workbook.createCellStyle();
+			greyTaskHeaderStyle.cloneStyleFrom( whiteTaskHeaderStyle );
+			greyTaskHeaderStyle.setFillForegroundColor( new XSSFColor( new Color( 237, 237, 237 ) ) );
+			greyTaskHeaderStyle.setFillPattern( FillPatternType.SOLID_FOREGROUND );
+			
+			XSSFCellStyle whiteWorkItemStyle = workbook.createCellStyle();
+			whiteWorkItemStyle.setFont( defaultFont );
+			whiteWorkItemStyle.setBorderLeft( BorderStyle.THIN );
+			whiteWorkItemStyle.setBorderRight( BorderStyle.MEDIUM );
+			whiteWorkItemStyle.setBorderTop( BorderStyle.THIN );
+			whiteWorkItemStyle.setBorderBottom( BorderStyle.THIN );
+			whiteWorkItemStyle.setAlignment( HorizontalAlignment.RIGHT );
+			
+			XSSFCellStyle greyWorkItemStyle = workbook.createCellStyle();
+			greyWorkItemStyle.cloneStyleFrom( whiteWorkItemStyle );
+			greyWorkItemStyle.setFillForegroundColor( new XSSFColor( new Color( 237, 237, 237 ) ) );
+			greyWorkItemStyle.setFillPattern( FillPatternType.SOLID_FOREGROUND );
+			
+			XSSFCellStyle whiteFeeHoursStyle = workbook.createCellStyle();
+			whiteFeeHoursStyle.cloneStyleFrom( whiteWorkItemStyle );
+			whiteFeeHoursStyle.setFont( italicFont );
+			
+			XSSFCellStyle greyFeeHoursStyle = workbook.createCellStyle();
+			greyFeeHoursStyle.cloneStyleFrom( greyWorkItemStyle );
+			greyFeeHoursStyle.setFont( italicFont );
 
-			CellStyle taskStyle = workbook.createCellStyle();
-			taskStyle.setFont( defaultFont );
-
-			CellStyle rateStyle = workbook.createCellStyle();
+			XSSFCellStyle rateStyle = workbook.createCellStyle();
 			rateStyle.setFont( defaultFont );
 			rateStyle.setAlignment( HorizontalAlignment.CENTER );
+			rateStyle.setDataFormat( (short) 8 );
+			rateStyle.setFillForegroundColor( new XSSFColor( new Color( 237, 237, 237 ) ) );
+			rateStyle.setFillPattern( FillPatternType.SOLID_FOREGROUND );
+			rateStyle.setBorderLeft( BorderStyle.THIN );
+			rateStyle.setBorderRight( BorderStyle.THIN );
+			rateStyle.setBorderBottom( BorderStyle.THIN );
 
-			CellStyle rateHeaderStyle = workbook.createCellStyle();
+			XSSFCellStyle rateHeaderStyle = workbook.createCellStyle();
 			rateHeaderStyle.cloneStyleFrom( rateStyle );
 			rateHeaderStyle.setAlignment( HorizontalAlignment.RIGHT );
-
+			rateHeaderStyle.setBorderRight( BorderStyle.MEDIUM );
+			
+			XSSFCellStyle staffStyle = workbook.createCellStyle();
+			staffStyle.setFont( boldFont );
+			staffStyle.setAlignment( HorizontalAlignment.CENTER );
+			staffStyle.setVerticalAlignment( VerticalAlignment.CENTER );
+			staffStyle.setWrapText(true);
+			staffStyle.setBorderLeft( BorderStyle.THIN );
+			staffStyle.setBorderRight( BorderStyle.THIN );
+			staffStyle.setBorderBottom( BorderStyle.THIN );
+			
+			XSSFCellStyle whiteTotalStyle = workbook.createCellStyle();
+			whiteTotalStyle.cloneStyleFrom( whiteWorkItemStyle );
+			whiteTotalStyle.setFont( boldFont );
+			
+			XSSFCellStyle greyTotalStyle = workbook.createCellStyle();
+			greyTotalStyle.cloneStyleFrom( greyWorkItemStyle );
+			greyTotalStyle.setFont( boldFont );
+			
+			XSSFCellStyle totalFeesHeaderStyle = workbook.createCellStyle();
+			totalFeesHeaderStyle.setFont( boldFont );
+			totalFeesHeaderStyle.setAlignment( HorizontalAlignment.CENTER );
+			totalFeesHeaderStyle.setVerticalAlignment( VerticalAlignment.CENTER );
+			totalFeesHeaderStyle.setBorderLeft( BorderStyle.MEDIUM );
+			totalFeesHeaderStyle.setBorderRight( BorderStyle.MEDIUM );
+			totalFeesHeaderStyle.setBorderTop( BorderStyle.MEDIUM );
+			totalFeesHeaderStyle.setBorderBottom( BorderStyle.MEDIUM );
+			totalFeesHeaderStyle.setWrapText( true );
+			
+			XSSFCellStyle totalHoursStyle = workbook.createCellStyle();
+			totalHoursStyle.cloneStyleFrom( totalFeesHeaderStyle );
+			totalHoursStyle.setVerticalAlignment( VerticalAlignment.BOTTOM );
+			totalHoursStyle.setWrapText( false );
+			
+			XSSFCellStyle totalFeesStyle = workbook.createCellStyle();
+			totalFeesStyle.cloneStyleFrom( totalFeesHeaderStyle );
+			totalFeesStyle.setDataFormat( (short) 8 );
+			totalFeesStyle.setVerticalAlignment( VerticalAlignment.BOTTOM );
+			totalFeesStyle.setWrapText( false );
+			
 			// Create sheet
 			XSSFRow header = sheet.createRow( (short) 0 );
 			sheet.addMergedRegion( new CellRangeAddress( 0, 0, 1, 27 ) );
@@ -134,8 +228,9 @@ public class SheetGenerator {
 			XSSFRow ratesRow = sheet.createRow( (short) 2 );
 			staffRow.createCell( 0 );
 			ratesRow.createCell( 0 );
-			staffRow.setHeightInPoints( (float) 50 );
+			staffRow.setHeightInPoints( (float) 100 );
 			staffRow.getCell( 0 ).setCellValue( "Staff" );
+			sheet.setColumnWidth( staffRow.getCell( 0 ).getColumnIndex(), 256 * 50 );
 			staffRow.getCell( 0 ).setCellStyle( greenStyle );
 			ratesRow.getCell( 0 ).setCellValue( "Rate" );
 			ratesRow.getCell( 0 ).setCellStyle( rateHeaderStyle );
@@ -143,10 +238,178 @@ public class SheetGenerator {
 			for ( int i = 1; i <= staffRates.size(); i++ ) {
 				staffRow.createCell( i );
 				staffRow.getCell( i ).setCellValue( staffRates.get( i - 1 )[ 0 ] );
+				staffRow.getCell( i ).setCellStyle( staffStyle );
 				ratesRow.createCell( i );
 				ratesRow.getCell( i ).setCellValue( Integer.parseInt( staffRates.get( i - 1 )[ 1 ] ) );
+				ratesRow.getCell( i ).setCellStyle( rateStyle );
 			}
-
+			staffRow.createCell( 78 );
+			staffRow.getCell( 78 ).setCellStyle( totalFeesHeaderStyle );
+			ratesRow.createCell( 78 );
+			ratesRow.getCell( 78 ).setCellStyle( totalFeesHeaderStyle );
+			sheet.addMergedRegion( new CellRangeAddress( 1, 2, 78, 78 ) );
+			staffRow.getCell( 78 ).setCellValue( "Task Subtotals" );
+			
+			
+			int row = 3;
+			
+			for ( int i = 0; i < tasks.size(); i++ ) {
+				XSSFRow task = sheet.createRow( row );
+				task.createCell( 0 );
+				task.getCell( 0 ).setCellValue( tasks.get( i )[ 1 ].equals( "" ) ? tasks.get( i )[ 0 ] : tasks.get( i )[ 1 ] );
+				if ( task.getRowNum() % 2 != 0 ) { // Odd rows are white
+					task.getCell( 0 ).setCellStyle( whiteTaskHeaderStyle );
+				} else { // Even rows are grey
+					task.getCell( 0 ).setCellStyle( greyTaskHeaderStyle );
+				}
+				for ( int col = 1; col <= 77; col++ ) {
+					task.createCell( col );
+					task.getCell( col ).setCellValue( "" );
+					if ( task.getRowNum() % 2 == 1 ) { // Odd rows are white
+						task.getCell( col ).setCellStyle( whiteTaskStyle );
+					} else { // Even rows are grey
+						task.getCell( col ).setCellStyle( greyTaskStyle );
+					}
+				}
+				row++;
+				
+				for ( int j = 2; j < tasks.get( i ).length; j++ ) {
+					XSSFRow workItem = sheet.createRow( row );
+					workItem.createCell( 0 );
+					workItem.getCell( 0 ).setCellValue( tasks.get( i )[ j ] );
+					if ( workItem.getRowNum() % 2 == 1 ) { // Odd rows are white
+						workItem.getCell( 0 ).setCellStyle( whiteWorkItemStyle );
+					} else { // Even rows are grey
+						workItem.getCell( 0 ).setCellStyle( greyWorkItemStyle );
+					}
+					for ( int col = 1; col <= 77; col++ ) {
+						workItem.createCell( col );
+						if ( workItem.getRowNum() % 2 == 1 ) { // Odd rows are white
+							workItem.getCell( col ).setCellStyle( whiteTaskStyle );
+						} else { // Even rows are grey
+							workItem.getCell( col ).setCellStyle( greyTaskStyle );
+						}
+					}
+					row++;
+				}
+				XSSFRow hours = sheet.createRow( row );
+				row++;
+				XSSFRow fee = sheet.createRow( row );
+				row++;
+				hours.createCell( 0 );
+				hours.getCell( 0 ).setCellValue( "Sub Total Hours" );
+				fee.createCell( 0 );
+				fee.getCell( 0 ).setCellValue( "Sub Total Fee" );
+				if ( hours.getRowNum() % 2 == 1 ) {
+					hours.getCell( 0 ).setCellStyle( whiteFeeHoursStyle );
+				} else {
+					hours.getCell( 0 ).setCellStyle( greyFeeHoursStyle );
+				}
+				if ( fee.getRowNum() % 2 == 1 ) {
+					fee.getCell( 0 ).setCellStyle( whiteFeeHoursStyle );
+				} else {
+					fee.getCell( 0 ).setCellStyle( greyFeeHoursStyle );
+				}
+				for ( int col = 1; col <= 77; col++ ) {
+					hours.createCell( col );
+					hours.getCell( col ).setCellFormula( "sum(" + columns[ col ] + ( row - tasks.get( i ).length ) + ":" + columns[ col ] + ( row - 2 ) + ")" );
+					if ( hours.getRowNum() % 2 == 1 ) {
+						hours.getCell( col ).setCellStyle( whiteTaskStyle );
+					} else {
+						hours.getCell( col ).setCellStyle( greyTaskStyle );
+					}
+					fee.createCell( col );
+					fee.getCell( col ).setCellFormula( columns[ col ] + "$3*" + columns[ col ] + ( row - 1 ) );
+					if ( fee.getRowNum() % 2 == 1 ) {
+						fee.getCell( col ).setCellStyle( whiteFeeStyle );
+					} else {
+						fee.getCell( col ).setCellStyle( greyFeeStyle );
+					}
+				}
+			}
+			
+			ArrayList<Integer> fees = new ArrayList<Integer>();
+			ArrayList<Integer> hours = new ArrayList<Integer>();
+			
+			for ( int i = 1; i <= sheet.getLastRowNum(); i++ ) {
+				String line = sheet.getRow( i ).getCell( 0 ).getStringCellValue();
+				if ( line.equals( "Sub Total Hours" ) ) {
+					hours.add( i + 1 );
+				} else if ( line.equals( "Sub Total Fee" ) ) {
+					fees.add( i + 1 );
+				}
+			}
+			
+			// Totals at the bottom of the sheet
+			XSSFRow hoursRow = sheet.createRow( row );
+			row++;
+			XSSFRow feesRow = sheet.createRow( row );
+			row++;
+			hoursRow.createCell( 0 );
+			hoursRow.getCell( 0 ).setCellValue( "TOTAL HOURS" );
+			feesRow.createCell( 0 );
+			feesRow.getCell( 0 ).setCellValue( "TOTAL FEE" );
+			
+			if ( hoursRow.getRowNum() % 2 == 1 ) {
+				hoursRow.getCell( 0 ).setCellStyle( whiteTotalStyle );
+				feesRow.getCell( 0 ).setCellStyle( greyTotalStyle );
+			} else {
+				feesRow.getCell( 0 ).setCellStyle( whiteTotalStyle );
+				hoursRow.getCell( 0 ).setCellStyle( greyTotalStyle );
+			}
+			for ( int i = 1; i <= 77; i++ ) {
+				hoursRow.createCell( i );
+				feesRow.createCell( i );
+				String col = columns[ i ];
+				String sumHours = "";
+				String sumFees = "";
+				for ( int j = 0; j < fees.size(); j++ ) {
+					if ( j != 0 ) {
+						sumHours += ",";
+						sumFees += ",";
+					}
+					sumHours += col + hours.get( j );
+					sumFees += col + fees.get( j );
+				}
+				hoursRow.getCell( i ).setCellFormula( "sum(" + sumHours + ")" );
+				feesRow.getCell( i ).setCellFormula( "sum(" + sumFees + ")" );
+				if ( hoursRow.getRowNum() % 2 == 1 ) {
+					hoursRow.getCell( i ).setCellStyle( whiteTaskStyle );
+					feesRow.getCell( i ).setCellStyle( greyFeeStyle );
+				} else {
+					feesRow.getCell( i ).setCellStyle( whiteFeeStyle );
+					hoursRow.getCell( i ).setCellStyle( greyTaskStyle );
+				}
+			}
+			
+			// Total totals at the bottom right corner
+			hoursRow.createCell( 78 );
+			feesRow.createCell( 78 );
+			hoursRow.getCell( 78 ).setCellFormula( "sum(B" + ( hoursRow.getRowNum() + 1 ) + ":BZ" + ( hoursRow.getRowNum() + 1 ) + ")" );
+			feesRow.getCell( 78 ).setCellFormula( "sum(B" + ( feesRow.getRowNum() + 1 ) + ":BZ" + ( feesRow.getRowNum() + 1 ) + ")" );
+			hoursRow.getCell( 78 ).setCellStyle( totalHoursStyle );
+			feesRow.getCell( 78 ).setCellStyle( totalFeesStyle );
+			
+			// Totals along the side of the sheet
+			for ( int i = 3; i <= feesRow.getRowNum() - 2; i++ ) {
+				sheet.getRow( i ).createCell( 78 );
+				sheet.getRow( i ).getCell( 78 ).setCellStyle( totalFeesStyle );
+			}
+			for ( int i = 0; i < fees.size(); i++ ) {
+				if ( i == 0 ) {
+					sheet.addMergedRegion( new CellRangeAddress( 3, fees.get( i ) - 1, 78, 78 ) );
+					sheet.getRow( 3 ).getCell( 78 ).setCellFormula( "sum(B" + ( fees.get( i ) ) + ":BZ" + ( fees.get( i ) ) + ")" );
+				} else {
+					sheet.addMergedRegion( new CellRangeAddress( fees.get( i - 1 ), fees.get( i ) - 1, 78, 78 ) );
+					sheet.getRow( fees.get( i - 1 ) ).getCell( 78 ).setCellFormula( "sum(B" + ( fees.get( i ) ) + ":BZ" + ( fees.get( i ) ) + ")" );
+				}
+			}
+			
+			// Resize columns
+			for ( int i = 1; i <= 78; i++ ) {
+				sheet.setColumnWidth( i, 3660 );
+			}
+			
 			// Write excel workbook
 			FileOutputStream fileOut = new FileOutputStream( filename );
 			workbook.write( fileOut );
@@ -182,18 +445,20 @@ public class SheetGenerator {
 
 	public static void main( String[] args ) {
 		// Allow user to select input csv file
-		JFileChooser fc = new JFileChooser();
-		fc.setCurrentDirectory( new File( "H:\\" ) );
-		int returnVal = fc.showOpenDialog( null );
-		if ( returnVal != JFileChooser.APPROVE_OPTION ) {
-			System.exit( 0 );
+		String filename;
+		if ( !args[ 0 ].equals( "debug" ) ) {
+			JFileChooser fc = new JFileChooser();
+			fc.setCurrentDirectory( new File( "H:\\" ) );
+			int returnVal = fc.showOpenDialog( null );
+			if ( returnVal != JFileChooser.APPROVE_OPTION ) {
+				System.exit( 0 );
+			}
+
+			filename = fc.getSelectedFile().getAbsolutePath();
+		} else { 
+			filename = "H:\\Labor Spreadsheet.csv";
 		}
-		String filename = fc.getSelectedFile().getAbsolutePath();
-
-		// Read Selected csv file
-		readCSV( filename );
-
-		generateSheet( filename );
+		generateSheet( filename, readCSV( filename ) );
 	}
 
 }
